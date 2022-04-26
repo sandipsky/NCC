6212827e633b776ba28cee22b250fc88f6b8b430
sandipsky <sandipshakya75@gmail.com>
2022-04-26 14:12:26 +0545

    added lab1 C# datatypes and conditional statements

---
+++ Conditions/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class program
+{
+    public static void Main()
+    {
+        Console.WriteLine("HEllo");
+    }
+
+    //TODO i wil do it later
+    //above line will put that comment in task list because it is special comment
+
+    static void ConditionalStatements()
+    {
+        Console.WriteLine("Enter the day");
+        var choice = Convert.ToInt32(Console.ReadLine());
+        //ToInt16 is short
+        string day = "";
+        if(choice==1)
+        {
+            day = "Sunday";
+        }
+        else if(choice==2)
+        {
+            day = "Monday";
+        }
+        else if(choice==3)
+        {
+            day = "Tuesday";
+        }
+        else if(choice==4)
+        {
+            day = "Wednesday";
+        }
+        else if(choice==5)
+        {
+            day = "Thrusday";
+            
+        }
+        else if(choice==6)
+        {
+            day = "Friday";
+        }
+        else if(choice==7)
+        {
+            day = "Saturday";
+        }
+        else
+        {
+            Console.WriteLine("Enter valid choice");
+        }
+
+
+
+        //ternary operator
+        //(condition) ? <True value> : <False value>
+
+        day=choice==1?"Sunday":choice==2?"Monday":choice==3?"Tuesday":choice==4?"Wednesday":choice==5?"Thrusday":choice==6?"Friday":choice==7?"Saturday":"Null";
+
+        switch (choice)
+        {
+            case 1:
+                day = "Sunday";
+                break;
+            case 2:
+                day = "Monday";
+                break;
+            case 3:
+                day = "Tuesday";
+                break;
+            case 4:
+                day = "Wednesday";
+                break;
+            case 5:
+                day = "Thrusday";
+
+                break;
+            case 6:
+                day = "Friday";
+                break;
+            case 7:
+                day = "Saturday";
+                break;
+            default:
+                Console.WriteLine("Enter valid choice");
+                break;
+        }
+
+
+    }
+}
+
---
+++ Datatypes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ncc.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            DataTypes();
+        }
+        static void DataTypes()
+        {
+            int i = 10;
+            i=20;
+            
+            char c = '2';
+            i=c;
+            long l=i;
+            float f = l;
+            double d = f;
+
+            f=(float)d;
+            l=(long)f;
+            i=(int)l;
+            c=(char)i;
+
+            //var a="1";
+            //var b = 2;
+            //a=b will not work
+
+            object o1="";
+            object o2=2;
+            o1=o2; //this will work
+
+
+            string str = "123";
+            int m = Convert.ToInt32(str);
+
+
+        }
+    }
+
+}
+
+//char => int => long => float => double
+
